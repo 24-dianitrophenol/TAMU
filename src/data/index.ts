3a506cc52cca5b5f0ed46ca754d0ae487d96e718
24-dianitrophenol <bongolealvin24@gmail.com>
2025-03-05 07:42:21 +0300

    as

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,7 @@
 import { Code, PenTool, Server, Smartphone } from 'lucide-react';
 
 export const images = {
-  logo: '/public/images/logo/logo.png',
+  logo: '/images/logo/logo.png',
   hero: [
     '/images/hero/hero1.jpg',
     '/images/hero/hero2.jpg',
